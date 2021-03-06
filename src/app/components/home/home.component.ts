14f45dbefec5e3718672a3b8bae5855746cfa6ee
ProjectGuardian <espinoza.cj3@gmail.com>
2021-03-06 22:48:41 +0800

    first commit, finish homepage

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,24 @@
+import { typeSourceSpan } from '@angular/compiler';
+import { Component, HostListener, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  fadeout = false;
+  @HostListener("document:scroll")
+  scrollFunction(){
+    if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+      this.fadeout=true;
+    }else{
+      this.fadeout=false;
+    }
+  }
+}
